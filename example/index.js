f8ecf233e808de0ad69eaa6e060f7731e42c4639
tiammosito <tammo@Tammos-MacBook-Pro.local>
2017-12-14 18:47:56 +0100

    pre v1

--- example/index.js
+++ example/index.js
@@ -4,52 +4,56 @@ import ReducerProvider from "react-reducer-component";
 
 const INC_COUNTER = "INC_COUNTER";
 const incCounter = () => {
-  return {
-    type: INC_COUNTER
-  };
+	return {
+		type: INC_COUNTER
+	};
 };
 
 const reducer = (state = {}, action) => {
-  if (action.type === INC_COUNTER) {
-    return {
-      ...state,
-      count: state.count + 1
-    };
-  } else return state;
+	if (action.type === INC_COUNTER) {
+		return {
+			...state,
+			count: state.count + 1
+		};
+	} else return state;
 };
 
 const initialState = {
-  count: 0
+	count: 0
 };
 
 const Counter = ({ reduce, count }) => (
-  <button onClick={() => reduce(incCounter())}>{count}</button>
+	<button onClick={() => reduce(incCounter())}>{count}</button>
 );
 
 const App = () => (
-  <div>
-    <h2>ReducerComponent {"\u2728"}</h2>
-    <ReducerProvider initialState={initialState} reducer={reducer}>
-      {(reduce, props) => {
-        return (
-          <div>
-            <Counter reduce={reduce} count={props.count} />
-          </div>
-        );
-      }}
-    </ReducerProvider>
-    <ReducerProvider initialState={initialState} reducer={reducer}>
-      {(reduce, props) => {
-        // you can use it for shared state too!
-        return (
-          <div>
-            <Counter reduce={reduce} count={props.count} />
-            <Counter reduce={reduce} count={props.count} />
-          </div>
-        );
-      }}
-    </ReducerProvider>
-  </div>
+	<div>
+		<h2>ReducerComponent {"\u2728"}</h2>
+		<ReducerProvider
+			initialState={initialState}
+			reducer={reducer}
+			render={(reduce, props) => {
+				return (
+					<div>
+						<Counter reduce={reduce} count={props.count} />
+					</div>
+				);
+			}}
+		/>
+		<ReducerProvider
+			initialState={initialState}
+			reducer={reducer}
+			render={(reduce, props) => {
+				// you can use it for shared state too!
+				return (
+					<div>
+						<Counter reduce={reduce} count={props.count} />
+						<Counter reduce={reduce} count={props.count} />
+					</div>
+				);
+			}}
+		/>
+	</div>
 );
 
 render(<App />, document.getElementById("root"));
